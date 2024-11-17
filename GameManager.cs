5e130ce2becc8c90c0f512796656177bada7b747
Rudy Emmerson <rudyemmerson@gmail.com>
2024-11-17 12:54:06 -0500

    Added professor's changes

--- GameManager.cs
+++ GameManager.cs
@@ -12,9 +12,21 @@ public class GameManager : MonoBehaviour
     public GameObject enemyOne;
     public GameObject cloud;
     public GameObject coin;
+    public GameObject powerup;
+
+    public AudioClip powerUp;
+    public AudioClip powerDown;
+
+    public int cloudSpeed;
+
+    private bool isPlayerAlive;
 
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI livesText;
+    public TextMeshProUGUI gameOverText;
+    public TextMeshProUGUI restartText;
+    public TextMeshProUGUI powerupText;
+    
     private int score;
     private int lives;
 
@@ -29,12 +41,14 @@ public class GameManager : MonoBehaviour
         lives = 3;
         scoreText.text = "Score: " + score;
         livesText.text = "Lives:" + lives;
+        isPlayerAlive = true;
+        cloudSpeed = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Restart();  
     }
     void CreateCoin()
     {
@@ -45,6 +59,13 @@ public class GameManager : MonoBehaviour
     {
         Instantiate(enemyOne, new Vector3(Random.Range(-9f, 9f), 7.5f, 0), Quaternion.Euler(0, 0, 180));
     }
+   
+    IEnumerator CreatePowerup()
+    {
+        Instantiate(powerup, new Vector3(Random.Range(-9f, 9f), 7.5f, 0), Quaternion.identity);
+        yield return new WaitForSeconds(Random.Range(3f, 6f));
+        StartCoroutine(CreatePowerup());
+    }
 
     void CreateSky()
     {
@@ -54,18 +75,48 @@ public class GameManager : MonoBehaviour
         }
     }
 
-public void EarnScore(int newScore)
-{
-    score = score + newScore;
-    scoreText.text = "Score: " + score;
-}
+    public void EarnScore(int newScore)
+    {
+        score = score + newScore;
+        scoreText.text = "Score: " + score;
+    }
 
-public void SeeLives(int loseLife)
+    public void SeeLives(int loseLife)
     {
         lives = lives - loseLife;
         livesText.text = "Lives: " + lives;
     }
 
+     public void GameOver()
+    {
+        isPlayerAlive = false;
+        CancelInvoke();
+        gameOverText.gameObject.SetActive(true);
+        restartText.gameObject.SetActive(true);
+        cloudSpeed = 0;
+    }
 
+    void Restart()
+    {
+        if(Input.GetKeyDown(KeyCode.R) && isPlayerAlive == false)
+        {
+            SceneManager.LoadScene("Game");
+        }
+    }
+
+    public void UpdatePowerupText(string whichPowerup)
+    {
+        powerupText.text = whichPowerup;
+    }
+
+    public void PlayPowerUp()
+    {
+        AudioSource.PlayClipAtPoint(powerUp, Camera.main.transform.position);
+    }
+
+    public void PlayPowerDown()
+    {
+        AudioSource.PlayClipAtPoint(powerDown, Camera.main.transform.position);
+    }
 }
 
